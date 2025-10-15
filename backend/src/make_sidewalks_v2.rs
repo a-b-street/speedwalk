02a07381046d6bcc8cecd0d718847b0426a42822
Dustin Carlino <dabreegster@gmail.com>
2025-10-15 15:18:04 +0100

    Skip new sidewalks when there are explicit tags saying there's only one side

--- backend/src/make_sidewalks_v2.rs
+++ backend/src/make_sidewalks_v2.rs
@@ -12,6 +12,8 @@ use utils::{OffsetCurve, Tags, aabb};
 
 use crate::{Kind, Speedwalk, edits::CreateNewGeometry};
 
+const BUFFER_DISTANCE: f64 = 3.0;
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum Side {
     Left,
@@ -23,8 +25,8 @@ impl Speedwalk {
     // - retain disconnected islands
     // - all roads, not just severances
     pub fn make_all_sidewalks_v2(&self) -> CreateNewGeometry {
-        let mut splitters = Vec::new();
-        let mut splitters_with_ways = Vec::new();
+        let mut roads = Vec::new();
+        let mut roads_with_ways = Vec::new();
         for (id, way) in &self.derived_ways {
             if way.kind == Kind::Sidewalk || way.kind == Kind::Other {
                 continue;
@@ -41,17 +43,17 @@ impl Speedwalk {
                 continue;
             }
 
-            splitters.push(way.linestring.clone());
+            roads.push(way.linestring.clone());
 
-            splitters_with_ways.push(GeomWithData::new(way.linestring.clone(), *id));
+            roads_with_ways.push(GeomWithData::new(way.linestring.clone(), *id));
         }
-        let splitters_mls = MultiLineString(splitters);
+        let roads_mls = MultiLineString(roads);
 
         // Buffer these all in one batch; it's much cleaner
         info!("Creating one big buffered blob");
-        let width = 3.0;
-        let subtract_polygons = splitters_mls
-            .buffer_with_style(BufferStyle::new(width).line_join(LineJoin::Round(width)));
+        let subtract_polygons = roads_mls.buffer_with_style(
+            BufferStyle::new(BUFFER_DISTANCE).line_join(LineJoin::Round(BUFFER_DISTANCE)),
+        );
 
         // Debugging
         #[cfg(target_arch = "wasm32")]
@@ -78,10 +80,18 @@ impl Speedwalk {
             "Splitting {} new sidewalks into smaller chunks aligned to roads",
             raw_new_sidewalks.len()
         );
-        let closest_splitter = RTree::bulk_load(splitters_with_ways);
+        let closest_road = RTree::bulk_load(roads_with_ways);
         let mut new_sidewalks = Vec::new();
         for sidewalk in raw_new_sidewalks {
-            for (ls, way, side) in split_new_sidewalks(sidewalk, &closest_splitter) {
+            for (ls, way, side) in split_new_sidewalks(sidewalk, &closest_road) {
+                // If the road lacks sidewalks on this side, skip
+                if self.derived_ways[&way].tags.is("sidewalk", "left") && side == Side::Right {
+                    continue;
+                }
+                if self.derived_ways[&way].tags.is("sidewalk", "right") && side == Side::Left {
+                    continue;
+                }
+
                 let mut tags = new_tags.clone();
                 tags.insert("tmp:closest_way", way.0.to_string());
                 tags.insert("tmp:side", format!("{side:?}"));
@@ -152,7 +162,7 @@ fn find_all_intersections(ls1: &LineString, ls2: &LineString) -> Vec<(Coord, usi
     hits
 }
 
-// For each point, find the closest splitter road that contributed to it. Chunk by that, including
+// For each point, find the closest road road that contributed to it. Chunk by that, including
 // the guess on which side of the road.
 fn split_new_sidewalks(
     full: LineString,
@@ -185,8 +195,8 @@ fn split_new_sidewalks(
 
 fn classify_side(pt: Point, ls: &LineString) -> Side {
     // TODO There's probably something much easier with Orient, but this works
-    let left = ls.offset_curve(-3.0).expect("offset failed");
-    let right = ls.offset_curve(3.0).expect("offset failed");
+    let left = ls.offset_curve(-BUFFER_DISTANCE).expect("offset failed");
+    let right = ls.offset_curve(BUFFER_DISTANCE).expect("offset failed");
     if distance(&left, pt).expect("snap failed") <= distance(&right, pt).expect("snap failed") {
         Side::Left
     } else {
