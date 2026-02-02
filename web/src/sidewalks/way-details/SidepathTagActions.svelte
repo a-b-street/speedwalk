2378c5c7ac194f8ff7677da74eeceb37185ea0de
Tobias <t@tobiasjordans.de>
2026-02-02 17:53:39 +0100

    WayDetails: Actions for sidepath like ways

---
+++ web/src/sidewalks/way-details/SidepathTagActions.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  let {
+    setTags,
+    highway,
+  }: {
+    setTags: (tags: Array<string[]>) => Promise<void>;
+    highway: "footway" | "path" | "cycleway";
+  } = $props();
+
+  const actions = $derived.by(() => {
+    switch (highway) {
+      case "path":
+        return [
+          { shortcut: "s", tags: [["is_sidepath", "yes"]] },
+          { shortcut: "n", tags: [["is_sidepath", "no"]] },
+          { shortcut: "c", tags: [["path", "crossing"]] },
+        ];
+      case "footway":
+        return [
+          { shortcut: "s", tags: [["footway", "sidewalk"]] },
+          { shortcut: "n", tags: [["is_sidepath", "no"]] },
+          { shortcut: "c", tags: [["footway", "crossing"]] },
+        ];
+      case "cycleway":
+        return [
+          { shortcut: "s", tags: [["is_sidepath", "yes"]] },
+          { shortcut: "n", tags: [["is_sidepath", "no"]] },
+          { shortcut: "c", tags: [["cycleway", "crossing"]] },
+        ];
+    }
+  });
+
+  async function onKeyDown(e: KeyboardEvent) {
+    const key = e.key.toLowerCase();
+    const action = actions.find((a) => a.shortcut === key);
+    if (action) {
+      await setTags(action.tags.map((p) => [...p]));
+    }
+  }
+</script>
+
+<svelte:window onkeydown={onKeyDown} />
+
+<ul class="list-group list-group-flush gap-2">
+  {#each actions as action}
+    <li class="list-group-item">
+      <button
+        type="button"
+        class="btn btn-secondary w-100"
+        onclick={() => setTags(action.tags.map((p) => [...p]))}
+      >
+        {action.tags.map((pair) => `${pair[0]}=${pair[1]}`).join(", ")}
+        <kbd>{action.shortcut}</kbd>
+      </button>
+    </li>
+  {/each}
+</ul>
