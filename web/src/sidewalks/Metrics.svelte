2d5b3f2cb268337242f2595eebab16b0bd1c3548
Tobias (FMC) <tobias@fixmycity.de>
2026-03-05 21:22:09 +0100

    Share labels between export and sidewalk

--- web/src/sidewalks/Metrics.svelte
+++ web/src/sidewalks/Metrics.svelte
@@ -3,7 +3,7 @@
   import LegendListWrapper from "../common/LegendListWrapper.svelte";
   import LegendListItem from "../common/LegendListItem.svelte";
   import { backend, mutationCounter, prettyPrintDistance, sum } from "../";
-  import { colors } from "./";
+  import { colors, kindLabels, roadKinds, nonRoadKinds } from "./";
   import type { Snippet } from "svelte";
 
   let {
@@ -11,18 +11,12 @@
     extraControls,
   }: { showKinds: Record<string, boolean>; extraControls: Snippet } = $props();
 
-  let roads = [
-    ["RoadWithSeparate", "With separate sidewalks"],
-    ["RoadWithTags", "With tagged sidewalks"],
-    ["RoadWithoutSidewalksExplicit", "Tagged as no sidewalks"],
-    ["RoadWithoutSidewalksImplicit", "Assumed as no sidewalks"],
-    ["RoadUnknown", "Totally unknown"],
-  ];
-  let nonRoads = [
-    ["Sidewalk", "Sidewalks"],
-    ["Crossing", "Crossings"],
-    ["Other", "Other"],
-  ];
+  let roads = $derived(
+    roadKinds.map((key) => [key, kindLabels[key]] as [string, string]),
+  );
+  let nonRoads = $derived(
+    nonRoadKinds.map((key) => [key, kindLabels[key]] as [string, string]),
+  );
 
   interface Metrics {
     total_length_meters: Record<keyof typeof colors, number>;
@@ -43,39 +37,36 @@
   }
 </script>
 
-<div class="card mb-3">
-  <div class="card-header">Roads</div>
-  <div class="card-body">
-    <div class="row mb-1">
-      {#each roads as [key, _]}
-        {@const length = metrics.total_length_meters[castKey(key)]}
-        <span
-          style:width={`${(100 * length) / total}%`}
-          style:height="100%"
-          style:background-color={colors[castKey(key)]}
-        ></span>
-      {/each}
-    </div>
-
-    <LegendListWrapper>
-      {#snippet children()}
-        {#each roads as [key, label]}
-          <LegendListItem color={colors[castKey(key)]} swatchClass="rectangle">
-            <Checkbox bind:checked={showKinds[key]}>
-              {label}: {prettyPrintDistance(
-                metrics.total_length_meters[castKey(key)],
-              )}
-            </Checkbox>
-          </LegendListItem>
-        {/each}
-      {/snippet}
-    </LegendListWrapper>
-
-    <div class="mb-3"></div>
-    {@render extraControls()}
-  </div>
+<h6 class="mb-2">Roads</h6>
+<div class="bar-wrapper mb-3">
+  {#each roads as [key, _]}
+    {@const length = metrics.total_length_meters[castKey(key)]}
+    <span
+      style:width={`${(100 * length) / total}%`}
+      style:height="100%"
+      style:background-color={colors[castKey(key)]}
+    ></span>
+  {/each}
 </div>
 
+<LegendListWrapper>
+  {#snippet children()}
+    {#each roads as [key, label]}
+      <LegendListItem color={colors[castKey(key)]} swatchClass="rectangle">
+        <Checkbox bind:checked={showKinds[key]}>
+          {label}: {prettyPrintDistance(
+            metrics.total_length_meters[castKey(key)],
+          )}
+        </Checkbox>
+      </LegendListItem>
+    {/each}
+  {/snippet}
+</LegendListWrapper>
+
+<div class="mb-3"></div>
+{@render extraControls()}
+
+<h6 class="mb-2 mt-3">Sidewalks</h6>
 <LegendListWrapper>
   {#snippet children()}
     {#each nonRoads as [key, label]}
@@ -91,14 +82,14 @@
 </LegendListWrapper>
 
 <style>
-  .row {
+  .bar-wrapper {
     display: flex;
     flex-wrap: nowrap;
     width: 100%;
     height: 30px;
   }
 
-  .row span {
+  .bar-wrapper span {
     flex-shrink: 1;
     min-width: 0;
   }
